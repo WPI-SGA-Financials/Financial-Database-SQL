6ad84eb57123d32af7c1abdfadf35d93dc0686c7
Kevin Bimonte <kcbimonte@wpi.edu>
2019-10-12 14:06:50 -0400

    FR Appeal Stuff

---
+++ Funding Request with Appeals.sql
@@ -0,0 +1,15 @@
+SELECT `Funding Requests`.`Name of Club`,
+       `Funding Requests`.`Fiscal Year`,
+       `Funding Requests`.`Dot Number`,
+       `Funding Requests`.`Amount Requested`,
+       `Funding Requests`.Decision,
+       `Funding Requests`.`Amount Approved`,
+       If(FRAppeals.`Appeal Date` IS NULL, 0, 1) AS `Appealed`,
+       IF(FRAppeals.`Appeal Date` IS NULL, 0, FRAppeals.`Appeal Date`) AS `Appeal Date`,
+       IF(FRAppeals.`Appeal Amount` Is NUll, 0, FRAppeals.`Appeal Amount`) AS `Appeal Amount`,
+       If(FRAppeals.`Decision` Is Null, '', FRAppeals.Decision) AS `Decision`,
+       If(FRAppeals.`Approved Appeal` Is Null, 0, FRAppeals.`Approved Appeal`) AS `Approved Appeal`,
+       If(FRAppeals.Notes Is NUll, '', FRAppeals.Notes) AS `Appeal Notes`
+From `Funding Requests`
+     LEFT JOIN FRAppeals on `Funding Requests`.ID = FR_ID
+WHERE `Fiscal Year` = 'FY 20';
