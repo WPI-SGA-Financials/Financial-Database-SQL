daffdbc3e2a3aa14bc2fd83f31312b4834d87bbe
Kevin Bimonte <8795118+kcbimonte@users.noreply.github.com>
2019-10-22 18:25:53 -0400

    Update Financial Transparency View.sql

--- Finacial Transparency Queries/Financial Transparency View.sql
+++ Finacial Transparency Queries/Financial Transparency View.sql
@@ -1,6 +1,5 @@
 # Creates the Financial Transparency View. In order to change fiscal year, change the @FiscalYear Variable
 # Created by Kevin Bimonte, Accountant 2019
-Set @FiscalYear = 'FY 19';
 CREATE Or REPLACE VIEW `Financial Transparency` AS
 Select Organizations.`Name of Club`,
        Classification,
@@ -12,10 +11,10 @@ Select Organizations.`Name of Club`,
 From Organizations
          LEFT JOIN `Organization Membership Numbers`
                    on Organizations.`Name of Club` = `Organization Membership Numbers`.`Name of Organization` And
-                      `Organization Membership Numbers`.`Fiscal Year` = @FiscalYear
-         LEFT JOIN Budgets B on Organizations.`Name of Club` = B.`Name of Club` AND B.`Fiscal Year` = @FiscalYear
+                      `Organization Membership Numbers`.`Fiscal Year` = 'FY 19'
+         LEFT JOIN Budgets B on Organizations.`Name of Club` = B.`Name of Club` AND B.`Fiscal Year` = 'FY 19'
          LEFT JOIN `Funding Requests` `F R`
-                   on Organizations.`Name of Club` = `F R`.`Name of Club` AND `F R`.`Fiscal Year` = @FiscalYear
+                   on Organizations.`Name of Club` = `F R`.`Name of Club` AND `F R`.`Fiscal Year` = 'FY 19'
 Where `Inactive?` = 0
-  And Organizations.Classification <> 'Department'
+  And Organizations.Classification <> 'Department' And Organizations.Classification <> 'Graduate'
 GROUP BY Organizations.`Name of Club`;
