dd6e779424e0299204388a522000fa5e0d3ef06f
Kevin Bimonte <kcbimonte@wpi.edu>
2019-12-06 14:18:44 -0500

    Updating Liability File to Calculate Totals

--- Funding Request Data/Liabilty.sql
+++ Funding Request Data/Liabilty.sql
@@ -14,3 +14,16 @@ FROM `Funding Requests`
 WHERE `Amount Approved` > 0
   AND `Fiscal Year` = @FiscalYear
 ORDER BY `Funding Date`, `Dot Number`;
+
+Set @FiscalYear = 'FY 20';
+Select count(*)                                                                   as `Heard Requests`,
+       sum(`Amount Approved`)                                                     as `Total Approved`,
+       sum(FRF.`Approved Amount`)                                                 as `RF Approved Amt`,
+       sum(If(Status = 'Approved', FRF.`Approved Amount`, `Amount Approved`))     AS `Total Liability`,
+       sum(if(`Workday Approved` = 'Yes', 1, 0))                                as `Approved Requests`,
+       sum(If(`Workday Approved` = 'Yes', 0,
+              If(Status = 'Approved', FRF.`Approved Amount`, `Amount Approved`))) AS `Total Workday Liability`
+From `Funding Requests`
+         Left JOIN FRReportForms FRF on `Funding Requests`.ID = FRF.FR_ID
+WHERE `Amount Approved` > 0
+  AND `Fiscal Year` = @FiscalYear
