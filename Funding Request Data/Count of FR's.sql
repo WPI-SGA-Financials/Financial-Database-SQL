03f25f015ce8dd4cc765b0b8c447e27c12ab6888
Kevin Bimonte <kcbimonte@wpi.edu>
2019-12-16 12:28:02 -0500

    Updating Count of FR's.sql

--- Funding Request Data/Count of FR's.sql
+++ Funding Request Data/Count of FR's.sql
@@ -2,7 +2,16 @@ SELECT `Organizations`.`Name of Club`,
        `Organizations`.`Account Number`,
        count(`Funding Requests`.`Dot Number`) as Amount
 FROM `sgadb`.`Organizations`
-         Inner Join `sgadb`.`Funding Requests` on `Organizations`.`Name of Club` = `Funding Requests`.`Name of Club`
-Where `Organizations`.`Inactive?` = 0
+         Left Join `sgadb`.`Funding Requests` on `Organizations`.`Name of Club` = `Funding Requests`.`Name of Club`
+Where `Organizations`.`Inactive?` = 0 and Classification != 'Mandatory Transfer'
+Group By `Organizations`.`Name of Club`
+Order BY count(`Funding Requests`.`Dot Number`) Desc;
+
+SELECT `Organizations`.`Name of Club`,
+       `Organizations`.`Account Number`,
+       count(`Funding Requests`.`Dot Number`) as Amount
+FROM `sgadb`.`Organizations`
+         Left Join `sgadb`.`Funding Requests` on `Organizations`.`Name of Club` = `Funding Requests`.`Name of Club`
+Where `Organizations`.`Inactive?` = 0 and Classification = 'Department'
 Group By `Organizations`.`Name of Club`
 Order BY count(`Funding Requests`.`Dot Number`) Desc;
