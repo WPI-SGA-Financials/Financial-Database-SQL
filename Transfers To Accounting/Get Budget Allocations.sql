ef06052feb79d6c6f58321144d9163e47febe16f
Kevin Bimonte <kcbimonte@wpi.edu>
2020-04-13 23:14:44 -0400

    Changing Budget Allocations

--- Transfers To Accounting/Get Budget Allocations.sql
+++ Transfers To Accounting/Get Budget Allocations.sql
@@ -1,9 +1,7 @@
-Set @FiscalYear = 'FY 20';
+Set @FiscalYear = 'FY 21';
 SELECT Budgets.`Name of Club`,
-       Organizations.`Account Number`,
+       `Account Number`,
        (Budgets.`Amount Proposed` + Budgets.`Approved Appeal`) as Allocation
-From Budgets,
-     Organizations
+From Budgets INNER Join Organizations O on Budgets.`Name of Club` = O.`Name of Club`
 Where Budgets.`Fiscal Year` = @FiscalYear
-  and Budgets.`Name of Club` = Organizations.`Name of Club`
   and (Budgets.`Amount Proposed` + Budgets.`Approved Appeal`) > 0;
