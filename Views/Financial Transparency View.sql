3b700c4f593f93bb40952989ae359526e7b8cd9e
Kevin C. Bimonte <8795118+kcbimonte@users.noreply.github.com>
2020-12-11 18:45:26 -0500

    Update Financial Transparency View.sql

--- Views/Financial Transparency View.sql
+++ Views/Financial Transparency View.sql
@@ -6,15 +6,17 @@ Select Organizations.`Name of Club`,
        IF(`Active Members` Is Null or `Active Members` = '', 'Not Provided', `Active Members`) AS 'Active Members',
        If(sum(`F R`.`Amount Approved`) Is NULL, 0,
           sum(`F R`.`Amount Approved`))                                                        As 'Funding Requests',
-       If((B.`Amount Proposed` + B.`Approved Appeal`) Is Null, 0,
-          (B.`Amount Proposed` + B.`Approved Appeal`))                                         As 'Budget'
+       If((Amount_Proposed + Approved_Appeal) Is Null, 0,
+          (Amount_Proposed + Approved_Appeal))                                         As 'Budget'
 From Organizations
          LEFT JOIN `Organization Membership Numbers`
                    on Organizations.`Name of Club` = `Organization Membership Numbers`.`Name of Organization` And
                       `Organization Membership Numbers`.`Fiscal Year` = 'FY 20'
-         LEFT JOIN Budgets B on Organizations.`Name of Club` = B.`Name of Club` AND B.`Fiscal Year` = 'FY 20'
+         LEFT JOIN Budget B on Organizations.`Name of Club` = B.`Name of Club` AND B.`Fiscal Year` = fnc_FiscalYear(now())
+         Left JOIN BudgetSection on B.ID = BudgetSection.B_ID
+         Left Join BudgetLineItem on BudgetSection.ID = BudgetLineItem.BS_ID
          LEFT JOIN `Funding Requests` `F R`
-                   on Organizations.`Name of Club` = `F R`.`Name of Club` AND `F R`.`Fiscal Year` = 'FY 20'
+                   on Organizations.`Name of Club` = `F R`.`Name of Club` AND `F R`.`Fiscal Year` = fnc_FiscalYear(now())
 Where `Inactive?` = 0
   And Organizations.Classification <> 'Department'
   And Organizations.Classification <> 'Graduate'
@@ -27,15 +29,17 @@ Select Organizations.`Name of Club`,
        IF(`Active Members` Is Null or `Active Members` = '', 'Not Provided', `Active Members`) AS 'Active Members',
        If(sum(`F R`.`Amount Approved`) Is NULL, concat('$', format(0, 2)),
           concat('$', format(sum(`F R`.`Amount Approved`), 2)))                                As 'Funding Requests',
-       If((B.`Amount Proposed` + B.`Approved Appeal`) Is Null, concat('$', format(0, 2)),
-          concat('$', format(sum(B.`Amount Proposed` + B.`Approved Appeal`), 2)))              As 'Budget'
+       If((Amount_Proposed + Approved_Appeal) Is Null, concat('$', format(0, 2)),
+          concat('$', format(sum(Amount_Proposed + Approved_Appeal), 2)))              As 'Budget'
 From Organizations
          LEFT JOIN `Organization Membership Numbers`
                    on Organizations.`Name of Club` = `Organization Membership Numbers`.`Name of Organization` And
                       `Organization Membership Numbers`.`Fiscal Year` = 'FY 20'
-         LEFT JOIN Budgets B on Organizations.`Name of Club` = B.`Name of Club` AND B.`Fiscal Year` = 'FY 20'
+         LEFT JOIN Budget B on Organizations.`Name of Club` = B.`Name of Club` AND B.`Fiscal Year` = fnc_FiscalYear(now())
+         Left JOIN BudgetSection on B.ID = BudgetSection.B_ID
+         Left Join BudgetLineItem on BudgetSection.ID = BudgetLineItem.BS_ID
          LEFT JOIN `Funding Requests` `F R`
-                   on Organizations.`Name of Club` = `F R`.`Name of Club` AND `F R`.`Fiscal Year` = 'FY 20'
+                   on Organizations.`Name of Club` = `F R`.`Name of Club` AND `F R`.`Fiscal Year` = fnc_FiscalYear(now())
 Where `Inactive?` = 0
   And Organizations.Classification <> 'Department'
   And Organizations.Classification <> 'Graduate'
